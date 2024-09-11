686e9802ea4d5c1ee23eeed1388944719c7e15de
JesseGonzalez01 <112505196+JesseGonzalez01@users.noreply.github.com>
2024-09-11 14:25:21 +0000

    pattern match

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,20 @@
+fn pattern_match_simple() {
+    let num = 3;
+    let letter = match num {
+        1 => 'A',
+        2 => 'B',
+        3 => (
+            println("Hey hey you choose number 3 right?");
+            'C'
+        ),
+        _ => '#', // rust will not guess
+    };
+
+
+
+    println!("{}", letter);
+}
 fn main() {
-    let mut money: u8 = 0;
-    money -= 1;
+    pattern_match_simple();
 
 }
