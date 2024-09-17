bd05ca5a13783506381de949070a18596cbb3167
JesseGonzalez01 <112505196+JesseGonzalez01@users.noreply.github.com>
2024-09-17 03:56:14 +0000

    concatenation

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,13 +1,13 @@
 fn concat_strings(s1: &String, s2: &String) -> String {
     // Your code here
-    let mut new_word: String = (*s1).clone()
-    new_word.push_str(string: s2);
+    let mut new_word: String = (*s1).clone();
+    new_word.push_str(s2);
     new_word
 }
 
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("World!");
-    let result = concat_strings(&s1, &s2);
+    let s1: String = String::from("Hello, ");
+    let s2: String = String::from("World!");
+    let result: String = concat_strings(&s1, &s2);
     println!("{}", result); // Should print: "Hello, World!"
 }
