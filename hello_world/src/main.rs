c7fff46c4a88bca508a30d1a936b2c459f3c85be
JesseGonzalez01 <112505196+JesseGonzalez01@users.noreply.github.com>
2024-09-30 17:57:15 +0000

    assignment attendance

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,47 +1,36 @@
-use std::io::{self, Read, Write, BufReader};
+use std::io::{self, Read, Write};
 use std::fs::File;
 
 struct Person {
     car: String,
 }
 
-fn reading_from_console() {
+fn reading_from_console() -> Person {
     let mut buffer = String::new();
 
     print!("What's your car? ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut buffer).unwrap();
     let car = buffer.trim().to_string();
-    buffer.clear();
 
-    // print!("How old are you? ");
-    // io::stdout().flush().unwrap();
-    // io::stdin().read_line(&mut buffer).unwrap();
-    // let age = buffer.trim().parse().unwrap();
+    Person { car }
+}
 
-    let person = Person { car };
+fn create_and_write_to_file(person: &Person) {
+    let mut file = File::create("user_info.txt").unwrap();
+    writeln!(file, "{}", person.car).unwrap();
+}
 
-    fn create_and_write_to_file() {
-        let mut file = File::create("user_info.txt").unwrap();
-        writeln!(file, person).unwrap();
-    }
-    
-    fn read_entire_file() {
-        let mut file = File::open("user_info.txt").unwrap();
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        println!("File contents:\n{}", contents);
-    }
-
-
-
-    // println!("Hi {}, you are {} years old!", person.name, person.age);
-
+fn read_entire_file() {
+    let mut file = File::open("user_info.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("File contents:\n{}", contents);
 }
 
 fn main() {
-    reading_from_console();
-    create_and_write_to_file();
+    let person = reading_from_console();
+    create_and_write_to_file(&person);
     println!("Reading entire file:");
     read_entire_file();
 }
