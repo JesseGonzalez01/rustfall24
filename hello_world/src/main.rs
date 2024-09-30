d26bfbfc312eb286e032eeeadd13ae45afae7c58
JesseGonzalez01 <112505196+JesseGonzalez01@users.noreply.github.com>
2024-09-30 15:43:18 +0000

    file reading

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,28 +1,47 @@
-use std::arch::asm;
+use std::io::{self, Read, Write, BufReader};
+use std::fs::File;
+
+struct Person {
+    car: String,
+}
+
+fn reading_from_console() {
+    let mut buffer = String::new();
+
+    print!("What's your car? ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let car = buffer.trim().to_string();
+    buffer.clear();
+
+    // print!("How old are you? ");
+    // io::stdout().flush().unwrap();
+    // io::stdin().read_line(&mut buffer).unwrap();
+    // let age = buffer.trim().parse().unwrap();
+
+    let person = Person { car };
+
+    fn create_and_write_to_file() {
+        let mut file = File::create("user_info.txt").unwrap();
+        writeln!(file, person).unwrap();
+    }
+    
+    fn read_entire_file() {
+        let mut file = File::open("user_info.txt").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        println!("File contents:\n{}", contents);
+    }
+
+
+
+    // println!("Hi {}, you are {} years old!", person.name, person.age);
+
+}
 
 fn main() {
-    let message = b"Hello, direct syscall!\n";
-
-    unsafe {
-        // write syscall
-        asm!(
-            "mov rax, 1",  // syscall number for write
-            "mov rdi, 1",  // file descriptor: 1 is stdout
-            "syscall",
-            in("rsi") message.as_ptr(),
-            in("rdx") message.len(),
-            out("rax") _,
-            out("rcx") _,
-            out("r11") _,
-            clobber_abi("system")
-        );
-
-        // exit syscall
-        asm!(
-            "mov rax, 60", // syscall number for exit
-            "xor rdi, rdi", // status code 0
-            "syscall",
-            options(noreturn)
-        );
-    }
+    reading_from_console();
+    create_and_write_to_file();
+    println!("Reading entire file:");
+    read_entire_file();
 }
