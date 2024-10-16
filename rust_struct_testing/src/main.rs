2893bba5707032e45258d3cdc60d12ee28550b39
JesseGonzalez01 <112505196+JesseGonzalez01@users.noreply.github.com>
2024-10-16 15:42:52 +0000

    creating test cases

---
+++ rust_struct_testing/src/main.rs
@@ -0,0 +1,11 @@
+
+
+mod student;
+mod major;
+
+
+fn main() {
+    let s = student::Student::new("John","CS");
+
+    println!("{:?}",s);
+}
---
+++ rust_struct_testing/src/major.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+pub enum Major {
+    ComputerScience,
+    ElectricalEngineering,
+    Undefined,
+}
+impl Major {
+    pub fn classify(major:&str) -> Self {
+        match major {
+            "CS" => Major::ComputerScience,
+            "EE" => Major::ElectricalEngineering,
+            _ => Major::Undefined,
+        }
+    }
+}
